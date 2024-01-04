021da0fd37b9805fcb15c876164ce20cc8279ae5
Naoya Takei <sol93ajmr34@gmail.com>
2024-01-04 13:03:02 +0900

    add users

---
+++ users/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from .models import CustomUser
+
+class CustomUserAdmin(admin.ModelAdmin):
+    # 一覧画面: 表示項目
+    list_display = (
+        "email",
+        "username",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    )
+    # 一覧画面: サイドバーフィルター
+    list_filter = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+
+    # 一覧画面: 検索ボックス
+    search_fields = ("email",)
+
+    # 一覧画面: ソート（降順ならフィールド名の先頭に-）
+    ordering = ("email",)
+
+    # 詳細画面: 表示項目
+    basic = ("username", "email", "password")
+    personal = ("last_name", "first_name", "date_joined")
+    auth = ("is_staff", "is_active")
+
+    fieldsets = (
+        ("BasicInfo", {"fields": basic}),
+        ("Personal", {"fields": personal}),
+        ("Auth", {"fields": auth}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ users/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        #emailがない場合のエラーメッセージ
+        if not email:
+            raise ValueError('メールアドレスは必須項目です。')
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+    
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email' #認証に使うフィールドの設定
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ['username'] #createsuperuserコマンド実行時に入力が必要なフィールド
