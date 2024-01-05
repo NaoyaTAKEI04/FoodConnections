6ce8b600730c77f311dfd460db0c38b2d8dea81a
Naoya Takei <sol93ajmr34@gmail.com>
2024-01-05 11:09:57 +0900

    Add:お店詳細ページの追加
    各飲食店の詳細ページを作成するため、以下を実行した。
    ・urls.pyの修正：ルーティング
    ・views.pyの修正：DetailViewの追加
    ・resutaurant_detail.htmlの作成
    ・resutaurant_list.htmlの修正：詳細ページへのリンクを追加

--- foodconnections/views.py
+++ foodconnections/views.py
@@ -5,3 +5,8 @@ class IndexView(generic.ListView):
     model = Restaurant
     template_name = 'foodconnections/restaurant_list.html'
     context_object_name = 'restaurant_list'
+
+class DetailView(generic.DetailView):
+    model = Restaurant
+    template_name = 'foodconnections/restaurant_detail.html'
+    context_object_name = 'restaurant'
