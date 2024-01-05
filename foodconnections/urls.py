c13949d3b0388b86d4fec1f1a236219c92abe387
Naoya Takei <sol93ajmr34@gmail.com>
2024-01-05 11:40:30 +0900

    Add:投稿フォームの追加
    新しいお店を追加するための投稿フォームを作成するため、以下を実行した。
    ・urls.pyの修正：ルーティング
    ・views.pyの修正：CreateViewの追加
    ・restaurant_form.htmlの作成
    ・restaurant_list.htmlの修正：投稿フォームへのリンクを追加

--- foodconnections/urls.py
+++ foodconnections/urls.py
@@ -6,4 +6,7 @@ app_name = 'foodconnections'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('detail/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('create/', views.CreateView.as_view(), name='create'),
+    #path('update/<int:pk>/', views.UpdateView.as_view(), name='update'),
+    #path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),
 ]
--- foodconnections/views.py
+++ foodconnections/views.py
@@ -1,5 +1,9 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.views import generic
 from .models import Category, Restaurant
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 class IndexView(generic.ListView):
     model = Restaurant
@@ -10,3 +14,14 @@ class DetailView(generic.DetailView):
     model = Restaurant
     template_name = 'foodconnections/restaurant_detail.html'
     context_object_name = 'restaurant'
+
+class CreateView(generic.edit.CreateView):
+    model = Restaurant
+    fields ='__all__'
+    template_name = 'foodconnections/restaurant_form.html'
+    success_url = reverse_lazy('foodconnections:index')
+
+    def form_valid(self, form):
+        restaurant_name = form.cleaned_data['name']
+        messages.success(self.request, f'"{restaurant_name}"を登録しました。')
+        return super().form_valid(form)
