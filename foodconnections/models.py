2b81b329cc722f39e451dd5d3a8579c3d73b6270
Naoya Takei <sol93ajmr34@gmail.com>
2024-01-05 10:50:08 +0900

    Add:ホーム画面（お店一覧）の追加
    ホーム画面（お店一覧）を追加するため、以下を実行した。
    ・settings.pyの修正：foodconnectionアプリの追加
    ・urls.pyの修正：ルーティング
    ・templatesフォルダの作成
    ・base.htmlの作成
    ・restaurant_list.htmlの作成
    ・マイグレーションフォルダの作成およびマイグレーション

--- foodconnections/models.py
+++ foodconnections/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
         on_delete=models.CASCADE,
     )
 
+    def __str__(self):
+        return self.name
+
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length = 100)
